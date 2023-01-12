a0f455f593918841f4d9bc0f702c5645ee30ce26
ivan-aleksic <ivan.aleksic582@gmail.com>
2023-01-12 16:53:31 +0100

    commit for unversioned files, changes at application properties for set up the database, pom.xml changes for mapstruct dependency

---
+++ src/test/java/hr/neos/carservicerestaleksa/CarServiceRestAleksaApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.neos.carservicerestaleksa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceRestAleksaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
